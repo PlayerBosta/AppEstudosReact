85d563d53e9fa9858f0479a62b6863b5b80645a5
PlayerBosta <131157222+PlayerBosta@users.noreply.github.com>
2025-04-01 12:54:45 -0300

    Versão final
    Adicionado todas as funcionalidades requisitadas

---
+++ atividade/Screens/Cronometro/CronometroPomodoreScreen.js
@@ -0,0 +1,53 @@
+// Filename - App.js
+
+import React, { useState, useRef, useEffect } from "react";
+import { View } from "react-native-web";
+import CustomButton from "../../Components/CustomButton/CustomButton";
+import { Text } from "react-native-web";
+
+
+
+const CronometroPomodoreScreen = () => {
+    const Ref = useRef(null);
+    const [tempo, setTempo] = useState(1200);
+    const [rodando, setRodando] = useState(false);
+
+const rodar = () => {
+    setRodando(!rodando);
+};
+
+    useEffect(() => {
+        let interval;
+        if (rodando) {
+            interval = setInterval(() => {
+                setTempo(prevTempo => prevTempo - 1);
+            }, 1000);
+        } else {
+            clearInterval(interval);
+        }
+
+        return () => clearInterval(interval);
+    }, [rodando]);
+
+    const resetar = () => {
+        setTempo(1200);
+        setRodando(false);
+    };
+
+    const formatar = (emSegundos) => {
+        const minutos =(String(Math.floor(emSegundos/60)).padStart(2,'0'));
+        const segundos =(String(Math.floor(emSegundos%60)).padStart(2,'0'));
+        return `${minutos}:${segundos}`
+    }
+
+    return (
+        <View style={StyleSheet.container}>
+            <Text style={StyleSheet.timeText}>{formatar(tempo)}</Text>
+            <View style={StyleSheet.buttonsContainer}>
+                <CustomButton title={rodando ? 'Pausar' : 'Iniciar'} onPress={rodar}> </CustomButton>
+                <CustomButton title="Resetar" onPress={resetar} />
+            </View>
+        </View>
+    )
+}
+export default CronometroPomodoreScreen;
